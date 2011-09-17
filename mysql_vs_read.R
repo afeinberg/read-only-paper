63b20b3e5cfca5a7686deb32c948501b36225ee5
Sam Shah <samshah@linkedin.com>
2011-09-16 17:09:04 -0700

    forgot plots

--- mysql_vs_read.R
+++ mysql_vs_read.R
@@ -1,5 +1,11 @@
 library(ggplot2)
 r <-read.csv("data/mysql_vs_read", sep="\t")
-s <- qplot(QPS, Median, data = r, geom=c("point", "line"), shape = Type, fontsize=14, theme_blank, xlab ="throughput (qps)", ylab="median latency (ms)") + opts(legend.position="top", legend.direction="horizontal") + scale_shape(name="")
+r <- melt(r, id=1:2)
+
+r <- subset(r, variable %in% c("Median", "X99th"))
+r$variable <- factor(r$variable)
+levels(r$variable) <- c("median", "99th quantile")
+
+s <- qplot(QPS, value, data = r, geom=c("point", "line"), shape = Type, fontsize=14, theme_blank, xlab ="throughput (qps)", ylab="latency (ms)") + opts(legend.position="top", legend.direction="horizontal") + scale_shape(name="") + facet_wrap(~variable, scale="free_y")
 pdf("images/mysql_vs_read.pdf", width=5, height=4)
 print(s, newpage=F)
