03c815657d7f7cae22267ace212088bf2f17b7f1
Roshan Sumbaly <rsumbaly@gmail.com>
2011-09-17 22:44:48 -0700

    Aaahhhh! Ok done...This time, really done! v0.2 out...

--- search_1node.R
+++ search_1node.R
@@ -6,10 +6,16 @@ mysql <- melt(r, 1, 4)
 xlab <- c(0, 30, 60, 90, 120, 150, 180)
 ylab <- c(0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8, 25.6)
 c <- rbind(interpolation,binary, mysql)
-s <- qplot(Minutes, value, data = c, shape = variable, fontsize=14, theme_blank, xlab ="time since swap (mins)", ylab ="median latency (ms)") + 
-	opts(legend.position="top", legend.direction="horizontal", size=0.5) + 
-	scale_shape(name="") + 
-	scale_x_continuous("time singe swap (mins)", breaks=xlab, labels=xlab) +
-	scale_y_log2("median latency (ms)", breaks=ylab, label=ylab)
+s <- qplot(Minutes, value, data = c, geom="line", linetype=variable, shape = variable, fontsize=14, theme_blank ) +
+         opts(legend.position="top", legend.direction="horizontal", size=0.5) +
+         scale_x_continuous("time since swap (mins)", breaks=xlab, labels=xlab) +
+         scale_y_log2("median latency (ms)", breaks=ylab, label=ylab) + scale_shape(legend=FALSE)+ scale_linetype_discrete(name="")
+# Old way of plotting
+# s <- qplot(Minutes, value, data = c, shape = variable, fontsize=14, theme_blank ) + 
+#	opts(legend.position="top", legend.direction="horizontal", size=0.5) + 
+#	scale_shape(name="") + 
+#	scale_x_continuous("time since swap (mins)", breaks=xlab, labels=xlab) +
+#	scale_y_log2("median latency (ms)", breaks=ylab, label=ylab)
+
 pdf("images/search_1node.pdf", width=5, height=4)
 print(s, newpage=F)
