8ad34b8de4b1e015d1967f50ef7030c2f798f21e
Sam Shah <samshah@linkedin.com>
2011-09-19 10:27:56 -0700

    cleaned up client side median latency plot

--- search_16node.R
+++ search_16node.R
@@ -3,6 +3,7 @@ r <-read.csv("data/search_16node", sep="\t")
 uniform <- melt(r, 1, 3)
 zipfian <- melt(r, 1, 5)
 c <- rbind(uniform, zipfian)
+levels(c$variable) <- c("Uniform", "Zipfian")
 xlab <- c(2,4,6,8,10)
 s <- qplot(GB, value, data = c, shape = variable, geom = c("line", "point"), fontsize=14, theme_blank) +
   scale_x_log2("output size (GB)", breaks=2^xlab, labels=2^xlab) +
